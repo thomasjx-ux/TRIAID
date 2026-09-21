fe0702bbfcb091768691e528c1ecb5bf0c511705
thomasjx-ux <thomasjx@gmail.com>
2026-09-21 18:57:08 +0800

    Update UI smoke architecture assertion for v0.12

--- finlab_v2/ui_smoke.py
+++ finlab_v2/ui_smoke.py
@@ -95,7 +95,7 @@ assert html.index('id="dailyTitle"') < html.index('id="overviewTitle"')
 assert html.index('id="overviewTitle"') < html.index('id="curveTitle"')
 
 s=status()
-assert s["architecture_version"]=="fin-evolution-lab@0.11.0"
+assert s["architecture_version"]=="fin-evolution-lab@0.12.0"
 assert s["strategy_registry_count"]==33
 
 d=daily("US")
