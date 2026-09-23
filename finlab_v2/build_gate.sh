b1d99a0e10b464e9c561d0e3cf5b8bbf37086121
thomasjx-ux <thomasjx@gmail.com>
2026-09-23 12:07:33 +0800

    test(fin): add preview guard smoke to build gate

--- finlab_v2/build_gate.sh
+++ finlab_v2/build_gate.sh
@@ -18,4 +18,5 @@ python backend_audit_regression.py
 python us_return_max_smoke.py
 python strategy_contract_smoke.py
 python production_smoke_contract_static.py
+python manual_preview_guard_smoke.py
 echo TRIAID_BUILD_GATE_PASS
